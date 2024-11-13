7ec322852737275dbf759dfc1b912341fd0a50e7
Lxie <85497179+lxie-0709@users.noreply.github.com>
2024-11-13 16:01:31 +0800

    Add files via upload

---
+++ bin/setup.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+#Refers to the tool execution name
+chmod +x telocomp_Filter.py telocomp_Assembly.py telocomp_maxmin.py telocomp_Complement.py telocomp_Collinearity.py
+echo "alias telocomp_Filter='python \"$(pwd)/telocomp_Filter.py\"'" >> ~/.bashrc
+echo "alias telocomp_Assembly='python \"$(pwd)/telocomp_Assembly.py\"'" >> ~/.bashrc
+echo "alias telocomp_maxmin='python \"$(pwd)/telocomp_maxmin.py\"'" >> ~/.bashrc
+echo "alias telocomp_Complement='python \"$(pwd)/telocomp_Complement.py\"'" >> ~/.bashrc
+echo "alias telocomp_Collinearity='python \"$(pwd)/telocomp_Collinearity.py\"'" >> ~/.bashrc
+
+
+
+
+
+
+
+
+
