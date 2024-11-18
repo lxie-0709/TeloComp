aae86322e0a7911ba67a90c6861a2586a776b11f
Lxie <85497179+lxie-0709@users.noreply.github.com>
2024-11-18 20:13:59 +0800

    Update setup.sh

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,12 +1,5 @@
 #!bin/bash
 #Refers to the tool execution name
-#chmod +x telocomp_Filter.py telocomp_Assembly.py telocomp_maxmin.py telocomp_Complement.py telocomp_Collinearity.py
-#echo "alias telocomp_Filter='python \"$(pwd)/telocomp_Filter.py\"'" >> ~/.bashrc
-#echo "alias telocomp_Assembly='python \"$(pwd)/telocomp_Assembly.py\"'" >> ~/.bashrc
-#echo "alias telocomp_maxmin='python \"$(pwd)/telocomp_maxmin.py\"'" >> ~/.bashrc
-#echo "alias telocomp_Complement='python \"$(pwd)/telocomp_Complement.py\"'" >> ~/.bashrc
-#echo "alias telocomp_Collinearity='python \"$(pwd)/telocomp_Collinearity.py\"'" >> ~/.bashrc
-
 ln -s $(pwd)/telocomp_Filter.py ./telocomp_Filter
 ln -s $(pwd)/telocomp_Assembly.py ./telocomp_Assembly
 ln -s $(pwd)/telocomp_maxmin.py ./telocomp_maxmin
@@ -15,7 +8,6 @@ ln -s $(pwd)/telocomp_Collinearity.py ./telocomp_Collinearity
 echo "export PATH=\$PATH:$(pwd)" >> ~/.bashrc
 
 #Installing python modules
-
 packages=("numpy" "pandas" "pysam" "biopython" "matplotlib")
 echo "Installing Python packages..."
 
