90d839b542ba79c307d49fe2d3af83a433f5e11c
Lxie <85497179+lxie-0709@users.noreply.github.com>
2025-08-17 12:50:57 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -48,4 +48,9 @@ exclude_patterns = []
 # HTML 输出配置
 # ----------------------------
 # 使用 Sphinx 默认主题
-html_theme = 'alabaster'
+# html_theme = 'alabaster'
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
